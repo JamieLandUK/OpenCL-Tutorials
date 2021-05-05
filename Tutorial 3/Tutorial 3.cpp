96a974161da05978351cd41260965702f8be4f1a
JamieLandUK <43765927+JamieLandUK@users.noreply.github.com>
2021-05-05 12:04:04 +0100

    more changes
    attempt on sorting, getting mean, min and max, basic concept for median, 1st and 3rd quartiles.

--- Tutorial 3/Tutorial 3.cpp
+++ Tutorial 3/Tutorial 3.cpp
@@ -30,11 +30,15 @@ int main(int argc, char **argv) {
 		//2.1 Select computing devices
 		cl::Context context = GetContext(platform_id, device_id);
 
+		// Get the device from the context which will be used later to decide our local size.
+		cl::Device device = context.getInfo<CL_CONTEXT_DEVICES>()[0];
+
 		//display the selected device
 		std::cout << "Running on " << GetPlatformName(platform_id) << ", " << GetDeviceName(platform_id, device_id) << std::endl;
 
 		//create a queue to which we will push commands for the device
-		cl::CommandQueue queue(context);
+		// need CL_QUEUE_PROFILING_ENABLE to receive ns time reports.
+		cl::CommandQueue queue(context, CL_QUEUE_PROFILING_ENABLE);
 
 		//2.2 Load & build the device code
 		cl::Program::Sources sources;
@@ -54,41 +58,68 @@ int main(int argc, char **argv) {
 			throw err;
 		}
 
-		typedef float mytype;
+		cl::Event total_profile; // Gets the kernel execution time for all kernels
+		cl::Event A_profile; cl::Event B_profile; cl::Event C_profile; // Execution time for single kernels
 
-		string temp;
-		string portion;
+		typedef int mytype;
 
-		std::ifstream file;
-		std::vector<mytype> A;
-		std::vector<string> vec;
+		// temporary values
+		string temp; // Holds entire strings
+		string portion; // Holds entire words
 
-		file.open("temp_lincolnshire_short.txt", ios::in);
+		std::ifstream file; // The input file.
+		std::vector<mytype> A; // Our main program vector
+		std::vector<string> vec; // A vector which holds each word from each line.
 
+		// "temp_lincolnshire_shorter.txt" is 51 records long.
+		// "temp_lincolnshire_short.txt" is 18732 records long.
+		// "temp_lincolnshire.txt" is 1873107 records long.
+		// Can be changed to point to a different file here.
+		file.open("temp_lincolnshire_shorter.txt", ios::in);
+
+		// If the file hasn't been opened properly
+		// (or doesn't exist)...
 		if (!file) {
+			// Tell the user.
 			std::cerr << "Could not read file!" << std::endl;
 		}
 
+		// file.good finds if the current line of the file exists.
+		// It is similar to .eof(), however that seems to have some issues.
 		while (file.good()) {
+			// Get the first line of our file and load it into temp.
 			while (std::getline(file, temp)) {
+				// Turn temp back into a proper string (string stream)
 				stringstream ss(temp);
+				// Which means we can create another while loop which splits
+				// the string into words, splitting on whitespace.
 				while (std::getline(ss, portion, ' ')) {
+					// Push the word into the vector.
 					vec.push_back(portion);
 				}
-				A.push_back(stof(vec.back()));
+				// We only want the last data item from the vector
+				// And it needs to be changed into a float and then pushed into the new vector.
+				A.push_back(stoi(vec.back()));
+				vec.clear(); // Clear the intermediary vector to save space.
 			}
 		}
 
-		std::cout << A << std::endl;
+		// Print the vector for testing.
+		std::cout << "Unsorted: " << A << std::endl;
 
-		file.close();
+		file.close(); // Close the file.
+		
+		
+		// We need the kernels to be defined here so we can get the work group info next.
+		cl::Kernel kernel_reduce_add = cl::Kernel(program, "reduce_add");
+		cl::Kernel kernel_bitonic_sort = cl::Kernel(program, "bitonic_sort");
+		cl::Kernel kernel_min_max = cl::Kernel(program, "min_max");
+
 
 		//Part 3 - memory allocation
-
-		//the following part adjusts the length of the input vector so it can be run for a specific workgroup size
-		//if the total input length is divisible by the workgroup size
-		//this makes the code more efficient
-		size_t local_size = 10;
+		// It doesn't matter which kernel we use here
+		// It should set the size to as many items are in the work group.
+		size_t local_size = kernel_reduce_add.getWorkGroupInfo<CL_KERNEL_PREFERRED_WORK_GROUP_SIZE_MULTIPLE>(device);
 
 		size_t padding_size = A.size() % local_size;
 
@@ -96,43 +127,74 @@ int main(int argc, char **argv) {
 		//insert additional neutral elements (0 for addition) so that the total will not be affected
 		if (padding_size) {
 			//create an extra vector with neutral values
-			std::vector<int> A_ext(local_size-padding_size, 0);
+			std::vector<mytype> A_ext(local_size-padding_size, 0);
 			//append that extra vector to our input
 			A.insert(A.end(), A_ext.begin(), A_ext.end());
 		}
 
 		size_t input_elements = A.size();//number of input elements
 		size_t input_size = A.size()*sizeof(mytype);//size in bytes
+		// How many elements in the vector compared to how many work items.
 		size_t nr_groups = input_elements / local_size;
 
 		//host - output
+		// The vector for the mean.
 		std::vector<mytype> B(input_elements);
 		size_t output_size = B.size()*sizeof(mytype);//size in bytes
+		// The vector which stores the minimum and maximum.
+		std::vector<mytype> C(input_elements);
 
 		//device - buffers
-		cl::Buffer buffer_A(context, CL_MEM_READ_ONLY, input_size);
-		cl::Buffer buffer_B(context, CL_MEM_READ_WRITE, output_size);
+		cl::Buffer buffer_A(context, CL_MEM_READ_ONLY, input_size); // The input
+		cl::Buffer buffer_B(context, CL_MEM_READ_WRITE, output_size); // The mean
+		cl::Buffer buffer_C(context, CL_MEM_READ_WRITE, output_size); // Min and Max
+		// buffer_C is set to 2 because there should only be two outputs.
 
 		//Part 4 - device operations
 
 		//4.1 copy array A to and initialise other arrays on device memory
 		queue.enqueueWriteBuffer(buffer_A, CL_TRUE, 0, input_size, &A[0]);
+		queue.enqueueWriteBuffer(buffer_C, CL_TRUE, 0, input_size, &C[0]);
 		queue.enqueueFillBuffer(buffer_B, 0, 0, output_size);//zero B buffer on device memory
 
 		//4.2 Setup and execute all kernels (i.e. device code)
-		cl::Kernel kernel_1 = cl::Kernel(program, "reduce_add_1");
-		kernel_1.setArg(0, buffer_A);
-		kernel_1.setArg(1, buffer_B);
-//		kernel_1.setArg(2, cl::Local(local_size*sizeof(mytype)));//local memory size
+		// SORTING
+		kernel_bitonic_sort.setArg(0, buffer_A);
+		// REDUCE ADD
+		kernel_reduce_add.setArg(0, buffer_A);
+		kernel_reduce_add.setArg(1, buffer_B);
+		// MIN AND MAX
+		kernel_min_max.setArg(0, buffer_A);
+		kernel_min_max.setArg(1, buffer_C);
 
 		//call all kernels in a sequence
-		queue.enqueueNDRangeKernel(kernel_1, cl::NullRange, cl::NDRange(input_elements), cl::NDRange(local_size));
+		queue.enqueueNDRangeKernel(kernel_reduce_add, cl::NullRange, cl::NDRange(input_elements), cl::NDRange(local_size));
+		queue.enqueueNDRangeKernel(kernel_bitonic_sort, cl::NullRange, cl::NDRange(input_elements), cl::NDRange(local_size));
+		queue.enqueueNDRangeKernel(kernel_min_max, cl::NullRange, cl::NDRange(input_elements), cl::NDRange(local_size));
+
+		// Profiling tasks
+		queue.enqueueNDRangeKernel(kernel_reduce_add, cl::NullRange, cl::NDRange(input_elements), cl::NDRange(local_size), NULL, &total_profile);
+		queue.enqueueNDRangeKernel(kernel_bitonic_sort, cl::NullRange, cl::NDRange(input_elements), cl::NDRange(local_size), NULL, &total_profile);
+		queue.enqueueNDRangeKernel(kernel_min_max, cl::NullRange, cl::NDRange(input_elements), cl::NDRange(local_size), NULL, &total_profile);
 
 		//4.3 Copy the result from device to host
+		queue.enqueueReadBuffer(buffer_C, CL_TRUE, 0, output_size, &C[0]);
 		queue.enqueueReadBuffer(buffer_B, CL_TRUE, 0, output_size, &B[0]);
+		queue.enqueueReadBuffer(buffer_A, CL_TRUE, 0, output_size, &A[0]);
 
-		std::cout << "A = " << A << std::endl;
-		std::cout << "B = " << B << std::endl;
+		//4.4 single kernel execution times
+		queue.enqueueWriteBuffer(buffer_A, CL_TRUE, 0, input_size, &A[0], NULL, &A_profile);
+		queue.enqueueWriteBuffer(buffer_B, CL_TRUE, 0, input_size, &B[0], NULL, &B_profile);
+		queue.enqueueWriteBuffer(buffer_C, CL_TRUE, 0, input_size, &C[0], NULL, &C_profile);
+
+		// Outputs
+		std::cout << "Sorted: " << A << std::endl; // The sorted vector
+		std::cout << "Mean: " << B[0] / A.size(); // The mean/average
+		std::cout << " Median: " << A[std::floor(A.size() / 2)]; // The median
+		std::cout << " Upper quartile: " << A[std::floor(A.size() * 0.75)]; // The upper quartile
+		std::cout << " Lower quartile: " << A[std::floor(A.size() * 0.25)]; // The lower quartile
+		std::cout << " Minimum: " << C[0]; // The min
+		std::cout << " Maximum: " << C[1] << std::endl; // The max
 	}
 	catch (cl::Error err) {
 		std::cerr << "ERROR: " << err.what() << ", " << getErrorString(err.err()) << std::endl;
